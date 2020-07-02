67f0de65dfefd4c577ff6548c60bca3739f7f4a3
nicholas570 <nicolas.mercier570@gmail.com>
2020-07-02 16:58:55 +0200

    adding colors.js and modifying css

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 import './App.css';
 
+import colors from './colors';
+import FadeInSection from './component/FadeInSection';
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <h1>All the CSS colors!</h1>
+
+      {colors.map((color) => (
+        <FadeInSection key={color}>
+          <div className="box" style={{ backgroundColor: color }}>
+            <span>{color}</span>
+          </div>
+        </FadeInSection>
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/FadeInSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function FadeInSection({ children }) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default FadeInSection;
+
+FadeInSection.propTypes = {
+  children: propTypes.element.isRequired,
+};
